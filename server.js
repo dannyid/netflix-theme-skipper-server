74e2097ba50ec917da2b29ef6262b98e4944ca4b
dannyid <danielshekhtman@gmail.com>
2014-12-15 03:55:36 -0500

    adding access control headers

--- server.js
+++ server.js
@@ -9,6 +9,11 @@ var express = require('express')
   , assert = require('assert')
   , dbUrl = 'mongodb://localhost:27017/myproject';
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 app.use( bodyParser.urlencoded( {extended: true} ) );
 app.use( bodyParser.json() );
 
